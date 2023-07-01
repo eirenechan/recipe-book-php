225652781584831317f297e2fe46f5694c2c03a2
Chi Yan Eirene CHAN <eirenechancy@gmail.com>
2023-07-01 14:15:23 +0200

    mysql config

--- db.php
+++ db.php
@@ -1,8 +1,12 @@
  <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "111";
-	$dbname = "online_recipes";
+	$servername = "eu-cdbr-west-03.cleardb.net";
+	$username = "bc141934716a88";
+	$password = "d35ee202";
+	$dbname = "heroku_db82d00dcb69272";
+	// $servername = "localhost";
+	// $username = "root";
+	// $password = "111";
+	// $dbname = "online_recipes";
 
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
